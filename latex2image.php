c312857e758e25a22951fc8e9d1a06fa2d27ddbc
Julius Adorf <jeadorf@gmail.com>
2010-05-22 07:59:02 +0200

    whitelisting feature deferred

--- latex2image.php
+++ latex2image.php
@@ -8,27 +8,18 @@ $template=$l2i.'/template.tex';
 $filename=$hash.'.png';
 $output=$l2i.'/cache/'.$filename;
 
-if (!file_exists($l2i.'/whitelist')) {
-    header('Content-type: image/png');
-    echo file_get_contents($l2i.'/deny.png');
-    exit;
-} else if (strpos(file_get_contents($l2i.'/whitelist'), $hash) === False) {
-    header('HTTP/1.0 403 Forbidden');
-    exit;
-} else {
-    if (!file_exists($output)) {
-        mkdir($tmpdir);
-        $echocmd='echo';
-        $echocmd.=' '.escapeshellarg($input);
-        $l2icmd=escapeshellarg($l2i.'/latex2image');
-        $l2icmd.=' -T '.escapeshellarg($tmpdir);
-        $l2icmd.=' -t '.escapeshellarg($template);
-        $l2icmd.=' -o '.escapeshellarg($output);
-        exec($echocmd.' | '.$l2icmd);
-        rmdir($tmpdir);
-    }
-
-    header('Content-type: image/png');
-    echo file_get_contents($l2i.'/cache/'.$filename);
+if (!file_exists($output)) {
+    mkdir($tmpdir);
+    $echocmd='echo';
+    $echocmd.=' '.escapeshellarg($input);
+    $l2icmd=escapeshellarg($l2i.'/latex2image');
+    $l2icmd.=' -T '.escapeshellarg($tmpdir);
+    $l2icmd.=' -t '.escapeshellarg($template);
+    $l2icmd.=' -o '.escapeshellarg($output);
+    exec($echocmd.' | '.$l2icmd);
+    rmdir($tmpdir);
 }
+
+header('Content-type: image/png');
+echo file_get_contents($l2i.'/cache/'.$filename);
 ?>
